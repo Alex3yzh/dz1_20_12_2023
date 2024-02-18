bb4fbea531addb6711099480de8da2b277b6cd8f
Alex3yzh <paltixt@gmail.com>
2024-02-18 19:22:28 +0200

    Changes

--- script.js
+++ script.js
@@ -1,96 +1,110 @@
-function Cat(breed, age, gender, color, name, photoUrl) {
-  this.breed = breed;
-  this.age = age;
-  this.gender = gender;
-  this.color = color;
-  this.name = name;
-  this.photoUrl = photoUrl;
-  this.isSleeping = false;
+class Cat {
+  constructor(
+    catName,
+    catAge,
+    catType,
+    catGender,
+    catColor,
+    catStatus,
+    catPhoto,
+    catPhotoSleep
+  ) {
+    this.name = catName;
+    this.age = catAge;
+    this.type = catType;
+    this.gender = catGender;
+    this.color = catColor;
+    if (typeof catStatus == "string") {
+      catStatus = false;
+    }
+    this.isSleep = catStatus;
+    this.photo = catPhoto;
+    this.photoS = catPhotoSleep;
+  }
 
-  this.eat = function () {
-    if (!this.isSleeping) {
-      console.log(this.name + " is eating.");
+  sleep = function () {
+    if (this.isSleep == true) {
+      console.log(this.name, "is sleeping");
     } else {
-      console.log(this.name + " is sleeping and can't eat now.");
+      console.log(this.name, "went to sleep");
+      this.isSleep = true;
     }
   };
-
-  this.sleep = function () {
-    this.isSleeping = true;
-    console.log(this.name + " is sleeping.");
+  wakeUp = function () {
+    if (this.isSleep == true) {
+      console.log(this.name, "woke up");
+      this.isSleep = false;
+    } else {
+      console.log(this.name, "no sleep now");
+    }
   };
-
-  this.wakeUp = function () {
-    this.isSleeping = false;
-    console.log(this.name + " woke up.");
+  eat = function () {
+    if (this.isSleep == true) {
+      console.log(this.name, "can't eat now");
+    } else {
+      console.log(this.name, "is eating");
+    }
   };
 }
 
-// Створення котиків
-var cats = [
-  new Cat(
-    "Persian",
-    3,
-    "Male",
-    "White",
-    "Fluffy",
-    "https://example.com/fluffy.jpg"
-  ),
-  new Cat(
-    "Siamese",
-    2,
-    "Female",
-    "Brown",
-    "Mittens",
-    "https://example.com/mittens.jpg"
-  ),
-  new Cat(
-    "Maine Coon",
-    4,
-    "Male",
-    "Black",
-    "Shadow",
-    "https://example.com/shadow.jpg"
-  ),
-  new Cat(
-    "Sphynx",
-    1,
-    "Female",
-    "Gray",
-    "Sphinxie",
-    "https://example.com/sphinxie.jpg"
-  ),
-];
+let cats = [];
 
-// Відображення кнопок для котів
-var catButtonsContainer = document.getElementById("cat-buttons");
-cats.forEach(function (cat, index) {
-  var button = document.createElement("button");
-  button.textContent = cat.name;
-  button.addEventListener("click", function () {
-    showCatInfo(index);
-  });
-  catButtonsContainer.appendChild(button);
+$(document).ready(function () {
+  cats = [
+    new Cat(
+      "Monika",
+      4,
+      "Mix",
+      "Male",
+      "Red",
+      true,
+      "img/cat1.png",
+      "img/cat1_sleep.jpeg"
+    ),
+    new Cat(
+      "Alise",
+      8,
+      "Scottish lop-eared",
+      "Male",
+      "Gray",
+      false,
+      "img/cat2.jpg",
+      "img/cat2_sleep.jpg"
+    ),
+    new Cat(
+      "Murka",
+      2,
+      "Siamese",
+      "Female",
+      "White and Black",
+      false,
+      "img/cat3.png",
+      "img/cat3_sleep.jpg"
+    ),
+  ];
 });
 
-// Функція для відображення інформації про кота
-function showCatInfo(index) {
-  var catInfoContainer = document.getElementById("cat-info-container");
-  catInfoContainer.innerHTML = "";
+$(".btns").click(function (e) {
+  let cat_id = e.target.id;
+  let btns = document.querySelectorAll(".btns input");
+  btns.forEach((btn) => {
+    btn.classList.remove("active_btn");
+  });
 
-  var cat = cats[index];
-  var catInfo = document.createElement("div");
-  catInfo.className = "cat-info";
-  catInfo.innerHTML = `
-      <h3>${cat.name}</h3>
-      <p>Порода: ${cat.breed}</p>
-      <p>Вік: ${cat.age}</p>
-      <p>Стать: ${cat.gender}</p>
-      <p>Колір шерстки: ${cat.color}</p>
-      <img src="${cat.photoUrl}" alt="${cat.name}">
-      <button onclick="cats[${index}].eat()">Ask to Eat</button>
-      <button onclick="cats[${index}].sleep()">Sleep</button>
-      <button onclick="cats[${index}].wakeUp()">Wake Up</button>
-    `;
-  catInfoContainer.appendChild(catInfo);
+  e.target.classList.add("active_btn");
+  console.log("Cat number:", cat_id);
+  console.log(cats[cat_id]);
+  cat_id = parseInt(e.target.id) - 1;
+  displayCat(cats[cat_id]);
+});
+
+function displayCat(cat) {
+  $("#img").html(`<img src="${cat.isSleep ? cat.photoS : cat.photo}" alt="">`);
+  $("#name").html("Name: " + cat.name);
+  $("#age").html("Age: " + cat.age + " year");
+  $("#race").html("Race: " + cat.type);
+  $("#gender").html("Gender: " + cat.gender);
+  $("#color").html("Color: " + cat.color);
+  $("#status").html("Status: " + (cat.isSleep ? "Sleeping" : "No sleep"));
+  nowCat = cat;
 }
